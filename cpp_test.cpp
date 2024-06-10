8067d1d6b11193c71fcb93390ed83a4a3281f1c7
JimmyNull <156345779+JimmyNull@users.noreply.github.com>
2024-06-10 15:06:00 +0000

    testing myself

--- cpp_test.cpp
+++ cpp_test.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 #include <stdio.h>
+#include <any>
+#include <string>
 
-int main(){
+template <typename T>
+class Node
+{
+private:
+    T data;
+public:
+    Node *next;
+
+public:
+    Node(const T &data) : data(data), next(nullptr){};
+
+    void getNodeData() { std::cout << data << std::endl; }
+    
+};
+
+template<std::string>
+void printNodes(Node<std::string> &head){
+    if (head.data == NULL)
+        std::cout << "There is nothing there my dude\r\n";
+    Node <std::string> cur = head; 
+    while (cur.next != NULL){
+        std::cout << cur << ", ";
+        cur = cur.next;
+    }
+}
+
+int main()
+{
     std::cout << "hello world\r\n";
+
+    Node<std::string> node_1("did it work");
+    //node_1.getNodeData();
+
+    Node<std::string> node_2("yes it did");
+
+    node_1.next = &node_2;
+
+    printNodes(node_1);
     return 0;
 }
